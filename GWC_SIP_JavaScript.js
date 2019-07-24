4004c57e84a2bb7dc747625ce0d67b8294ffec97
seaynhs <52467222+seaynhs@users.noreply.github.com>
2019-07-24 10:58:01 -0400

    Add files via upload

---
+++ GWC_SIP_JavaScript.js
@@ -0,0 +1,22 @@
+function resetHide() {
+  document.getElementById("lettersAthruI").style.display="none";
+  document.getElementById("lettersJthruQ").style.display="none";
+  document.getElementById("lettersRthruZ").style.display="none";
+  document.getElementById("numbers1thru10").style.display="none";
+  document.getElementById("numbers11thru20").style.display="none";
+}
+function toggleShowLetterSet1() {
+document.getElementById("lettersAthruI").style.display="block";
+}
+function toggleShowLetterSet2() {
+document.getElementById("lettersJthruQ").style.display="block";
+}
+function toggleShowLetterSet3() {
+document.getElementById("lettersRthruZ").style.display="block";
+}
+function toggleShowNumberSet1() {
+document.getElementById("numbers1thru10").style.display="block";
+}
+function toggleShowNumberSet2() {
+document.getElementById("numbers11thru20").style.display="block";
+}
